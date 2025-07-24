365f2fdcbb13ae98d875134060123baeb4da2f03
Kelvin <haminhhoang19@gmail.com>
2025-07-24 17:24:35 +1000

    navbar added items

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,7 +1,33 @@
+import { Button } from '@/components/ui/button';
 import { Montserrat } from 'next/font/google';
 import Link from 'next/link';
 
-const inter = Montserrat({ subsets : ['latin']});
+const inter = Montserrat({ subsets : ['latin'], weight: ["700"],});
+
+interface NavbarItemsProps {
+    href: string
+    children: React.ReactNode
+    isActive?: boolean
+}
+const NavbarItems = ({
+    href,
+    children,
+    isActive
+}: NavbarItemsProps) => {
+    return (
+    <Button variant={'outline'} className='border-0 bg-transparent rounded-full 
+    hover:bg-amber-100 hover:text-amber-700 hover:shadow-md hover:scale-105 transition-all'>
+        {children}
+    </Button>
+    )
+}
+
+const navbaritem = [
+    {href : "/", children: "Home"},
+    {href : "/about", children: "About"},
+    {href : "/Pricing", children: "Pricing"},
+    {href : "/Contact", children: "Contact"},
+]
 
 export const Navbar = () => {
     return (
@@ -9,6 +35,13 @@ export const Navbar = () => {
             <Link href='/' className='flex items-center'>
                 <span className={`text-5xl font-semibold ${inter.className}`}>Coffee</span>
             </Link>
+            <div className='flex items-center gap-x-4 lg:flex'>
+                {navbaritem.map((item, idx) => (
+                 <NavbarItems key={item.href + idx} href={item.href}>
+                {item.children}
+                 </NavbarItems>
+                ))}
+            </div>
         </nav>
     );
 };
