293353fa6b9b262560f502b6a76389b9bb2d91a0
Kelvin <haminhhoang19@gmail.com>
2025-08-19 14:59:25 +1000

    Resolve Module not found

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,7 +1,7 @@
 import type { CollectionConfig } from 'payload'
 
 export const Categories: CollectionConfig = {
-  slug: "categories",
+  slug: 'categories',
   fields: [
     {
       name: "name",
