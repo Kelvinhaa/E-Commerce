b7bf1c4b17c387f7c8c984250e0dbd214867c540
Kelvin <haminhhoang19@gmail.com>
2025-08-30 21:40:13 +1000

    Linking about page

--- src/app/(app)/(home)/about/page.tsx
+++ src/app/(app)/(home)/about/page.tsx
@@ -3,15 +3,19 @@ import Image from "next/image";
 const Page = () => {
     return (
         <div>
-            <title>About</title>
-            <h1 className="flex flex-col items-center">A place where you can share your knowledge
-            </h1>
-
-            <Image src="/coffee_chat.webp"
-            alt="Image of coffee chat"
-            width={400}
-            height={400}
-            />
+            <main>
+                <header>
+                    <h1>About BrewMarket</h1>
+                </header>
+            <section>
+                 <img src="/coffee_chat.webp" alt="About BrewMarket" 
+                 width={500} height={500}/>
+            <p>
+            BrewMarket connects coffee shop owners with coffee enthusiasts. 
+            Buy premium beans, equipment, and everything you need for the perfect brew.
+            </p>
+            </section>
+            </main>
         </div>
     );
 }
